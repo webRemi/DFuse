a3514e889213a0cb2bcb436e0d54d38206e6e89c
4$X <114998095+webRemi@users.noreply.github.com>
2025-03-20 20:31:47 +0100

    Create dllmessage.c

---
+++ examples/dllmessage.c
@@ -0,0 +1,31 @@
+#include <Windows.h>
+#include <stdio.h>
+
+void Payload() {
+    STARTUPINFO si = { 0 };
+    PROCESS_INFORMATION pi;
+    
+    si.cb = sizeof(si);
+    //MessageBoxA(NULL, "NTDLL injection till crazy...", "Wow !", MB_OK | MB_ICONINFORMATION);
+    CreateProcessA("C:\\windows\\system32\\calc.exe", NULL, NULL, NULL, 0, NORMAL_PRIORITY_CLASS, NULL, NULL, &si, &pi);
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  dwReason,
+                       LPVOID lpReserved
+                     )
+{
+    switch (dwReason)
+    {
+    case DLL_PROCESS_ATTACH: {
+        Payload();
+        break;
+    };
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
